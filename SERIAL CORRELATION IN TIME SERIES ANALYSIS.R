9a6619a48363e92fd056fedc29e3317126b9a798
T H <hutianyou@live.cn>
2016-05-25 19:55:15 +1200

    update

--- SERIAL CORRELATION IN TIME SERIES ANALYSIS.R
+++ SERIAL CORRELATION IN TIME SERIES ANALYSIS.R
@@ -88,3 +88,55 @@ acf(amznrt, na.action = na.omit)
 amznrt.ar = ar(amznrt, na.action = na.omit)
 amznrt.ar$order
 amznrt.ar$ar
+amznrt.ar$asy.var.coef[1,1]
+amznrt.ar$ar[1] + c(-1.96,1.96)*sqrt(amznrt.ar$asy.var.coef[1,1])
+amznrt.ar$ar[2] + c(-1.96,1.96)*sqrt(amznrt.ar$asy.var.coef[1,1])
+#S&P500 US Equity Index
+getSymbols("^GSPC")
+GSPC
+plot(Cl(GSPC))
+gspcrt = diff(log(Cl(GSPC)))
+plot(gspcrt)
+acf(gspcrt, na.action = na.omit) # There is evidence of long memory
+gspcrt.ar = ar(gspcrt, na.action = na.omit)
+gspcrt.ar$order # 20, it tells taht there is likely a lot more complexity in the serial correlation than 
+#a simple liner model of past prices
+
+# 5.AUTOREGRESSIVE MOVING AVERAGE ARMA(P, Q) MODELS FOR TIME SERIES ANALYSIS - PART 2
+# Simulations and Correlograms
+#MA(1)
+set.seed(1)
+x <- w <- rnorm(100)
+for (t in 2:100) x[t] <- w[t] + 0.6*w[t-1]
+layout(1:2)
+plot(x, type = 'l')
+acf(x)
+x.ma <- arima(x, order=c(0,0,1))
+x.ma
+0.6023 + c(-1.96,1.96)*0.0827 
+# 0.440208 0.764392, 0.6 is in this interval, thus this model is a good fit.
+# Let's make it a function
+makeMA1 = function(para){
+  set.seed(1)
+  x <- w <- rnorm(100)
+  for (t in 2:100) x[t] <- w[t] + para*w[t-1]
+  layout(1:2)
+  plot(x, type = 'l')
+  acf(x)
+  return (x)
+}
+x <- makeMA1(-0.6)
+x.ma <- arima(x, order=c(0,0,1))
+x.ma
+-0.7298 + c(-1.96, 1.96)*0.1008
+# MA(3)
+
+
+
+
+
+
+
+
+
+
