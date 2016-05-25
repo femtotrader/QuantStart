dcb0ef54d75092a9cdeb316d30113841ac55a9ec
T H <hutianyou@live.cn>
2016-05-26 00:31:31 +1200

    update

--- SERIAL CORRELATION IN TIME SERIES ANALYSIS.R
+++ SERIAL CORRELATION IN TIME SERIES ANALYSIS.R
@@ -130,8 +130,12 @@ x.ma <- arima(x, order=c(0,0,1))
 x.ma
 -0.7298 + c(-1.96, 1.96)*0.1008
 # MA(3)
-
-
+set.seed(3)
+x <- w <- rnorm(1000)
+for (t in 4:1000) x[t] = w[t] + 0.6*w[t-1] + 0.4*w[t-2] + 0.3*w[t-3]
+layout(1:2)
+plot(x, type = 'l')
+acf(x)
 
 
 
